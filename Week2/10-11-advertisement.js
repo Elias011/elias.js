f404735a3efb241e5a55f89c168adc75278775cd
Elias011 <elias.shkshb1991@gmail.com>
2017-09-06 01:43:46 +0200

    advertisement
    Use the list of vehicles to write an advertisement. So that it prints something like: "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.". (Hint: use a for loop.)
    
    What if you add one more vehicle to the list, can you have that added to the advertisement without changing the code for question 7?

---
+++ Week2/10-11-advertisement.js
@@ -0,0 +1,13 @@
+let garage = {
+    ad1: "Motor,",
+    ad2: "Cars,",
+    ad3: "caravan ",
+    ad4: "and bikes"
+};
+
+let x = " ";
+for(let i in garage){
+  x = x + garage[i];
+}
+ console.log( "Amazing Joe's Garage, we service" + " " + x); 
+ 
