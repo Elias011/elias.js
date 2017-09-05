8cc9df359f97cc231c1bb23cc5be707bd0733cef
Elias011 <elias.shkshb1991@gmail.com>
2017-09-05 11:37:22 +0200

    function-vehicleType
    Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike. And prints "a blue motorbike" for example when called as vehicleType("blue", 2)

---
+++ Week2/4-function-vehicleType.js
@@ -0,0 +1,13 @@
+function vehicleType(color, code){
+  if (code === 1){
+      console.log("a" + " " + color + " " + "car");
+  }else if (code === 2) {
+      console.log("a" + " " + color + " " + "motorBike");
+  }else {
+      console.log("a" + " " + color + " " + "something");
+  }
+    return;
+}
+
+
+vehicleType("blue", 2);
