aa61a196eeb5aa902bc003700edd20d88b8b2979
Elias011 <elias.shkshb1991@gmail.com>
2017-09-05 23:23:23 +0200

    function-vehicle
    Change the function vehicle to use the list of question 4. So that vehicle("green", 3, 1) prints "a green new caravan".

---
+++ Week2/9-function-vehicle.js
@@ -0,0 +1,20 @@
+function vehicle (color, code, age){
+   //Used
+    if (code === 1 && age < 0){
+       console.log("a" + " " + color + " " + "new car");
+   }else if (code === 2 && age < 0){
+       console.log("a" + " " + color + " " + "new motor");
+   }else if (code === 3 && age < 0){
+       console.log("a" + " " + color + " " + "new caravan");
+   }
+    //New
+    if (code === 1 && age === 0){
+       console.log("a" + " " + color + " " + "used car");
+   }else if (code === 2 && age === 0){
+        console.log("a" + " " + color + " " + "used motor");
+   }else if (code === 3 && age === 0){
+        console.log("a" + " " + color + " " + "used caravan");
+   }
+    return;
+}
+vehicle('green', 3, 0);
